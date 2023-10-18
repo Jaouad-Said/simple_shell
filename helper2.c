4b32c8a9e73a01338066380ff387b65ae7bf5580
Jaouad-Said <73340486+Jaouad-Said@users.noreply.github.com>
2023-10-18 02:36:52 +0100

    Update helper2.c

--- helper2.c
+++ helper2.c
@@ -53,14 +53,14 @@ int custom_atoi(char *s)
 }
 
 /**
- * custom_strcpy - Copies the string pointed to by src,
+ * customstring_copy - Copies the string pointed to by src,
  * including the terminating null byte (\0),
  * to the buffer pointed to by dest.
  * @dest: Copy source to this buffer.
  * @src: This is the source to copy.
  * Return: Copy of the original source.
  */
-char *custom_strcpy(char *dest, char *src)
+char *customstring_copy(char *dest, char *src)
 {
 	int i, len;
 
